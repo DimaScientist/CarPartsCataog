759f62e5b3de8d670ec24beb832003992e67e386
DimaScientist <61016327+DimaScientist@users.noreply.github.com>
2020-03-17 18:00:51 +0400

    Create controller for partnum

--- src/main/java/Controllers/RestControllerPartsVehicles.java
+++ src/main/java/Controllers/RestControllerPartsVehicles.java
@@ -1,8 +1,20 @@
 package Controllers;
 
 
+import Service.JDBCAnswerPartsVehicles;
+import Tables.PartsVehicles;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class RestControllerPartsVehicles {
+
+    @GetMapping(value = "/partsvehicles")
+    public @ResponseBody List<PartsVehicles> getData(){
+        JDBCAnswerPartsVehicles answer = new JDBCAnswerPartsVehicles();
+        return answer.getPartsVehiclesList();
+    }
 }
