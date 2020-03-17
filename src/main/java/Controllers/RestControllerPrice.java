af95e1ec5fa43afb06a9508b055b55b95f9549d4
DimaScientist <61016327+DimaScientist@users.noreply.github.com>
2020-03-17 18:03:42 +0400

    Create controller for price

--- src/main/java/Controllers/RestControllerPrice.java
+++ src/main/java/Controllers/RestControllerPrice.java
@@ -1,7 +1,19 @@
 package Controllers;
 
+import Service.JDBCPrice;
+import Tables.Price;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class RestControllerPrice {
+
+    @GetMapping(value = "/price")
+    public @ResponseBody List<Price> getData(){
+        JDBCPrice answer = new JDBCPrice();
+        return answer.getPriceList();
+    }
 }
